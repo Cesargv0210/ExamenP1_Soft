c8784ef898eda393d15f06415e1ec50bfeed8b39
César <ceangarc@espol.edu.ec>
2025-11-19 15:12:03 -0500

    Desarrollo de la funcion caudal

--- View/main.py
+++ View/main.py
@@ -1,1 +1,5 @@
-#hola
+#%% #importamos las librerias
+from poes.model.poes import poes
+import numpy as np
+from scipy.stats import norm, lognorm, expon, triang, uniform
+import matplotlib.pyplot as plt
